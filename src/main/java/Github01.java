5676203ea43cdce3f360f3ea948f97e2c0532b61
“Smitha” <bonasmitha.behura@gmail.com>
2024-05-03 11:11:42 +0200

    mycommit

---
+++ src/main/java/Github01.java
@@ -0,0 +1,20 @@
+public class Github01 {
+
+    public static void main(String[] args) {
+        //Codes version 1
+        int x = 5;
+        int y = 7;
+        System.out.println(x+y);
+        System.out.println("Done codes for version 1");
+
+        int m = 12;
+        int k = 11;
+        System.out.println(m+k);
+        System.out.println("Done codes for version 2");
+
+        int t = 12;
+        int a = 11;
+        System.out.println(t+a);
+        System.out.println("Done codes for version 3");
+    }
+}
